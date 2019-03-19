c201dcccc0e71963e92161a779e3c63e6489c766
varun93 <1hvarun@gmail.com>
2019-03-19 14:10:11 -0400

    changed build config

--- humanPlayerUI/src/App.js
+++ humanPlayerUI/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import Board from "./Board";
-import TurnChooser from "./TurnChooser";
+// import TurnChooser from "./TurnChooser";
 import "./App.css";
 
 class App extends Component {
