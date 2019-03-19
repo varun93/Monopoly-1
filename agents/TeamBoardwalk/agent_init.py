d14ad3601645724e4315d263ac7abac12ef5ff6a
varun93 <1hvarun@gmail.com>
2019-03-19 14:56:04 -0400

    harcoded server url

--- agents/TeamBoardwalk/agent_init.py
+++ agents/TeamBoardwalk/agent_init.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 	else:
 		from team_boardwalk import *
 		# pass
-		url = environ.get("CBURL", u"ws://127.0.0.1:8080/ws")
+		url = environ.get("CBURL", u"ws://18.222.174.193:80/ws")
 		if six.PY2 and type(url) == six.binary_type:
 			url = url.decode('utf8')
 		realm = environ.get('CBREALM', u'realm1')
