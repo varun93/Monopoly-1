a8a58d6a504354f69fa4d0d188f9b4c853ee2535
varun93 <1hvarun@gmail.com>
2019-03-28 18:44:25 -0400

    added autobahn js

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,106 +1,145 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import Board from "./Board";
+import Autobahn from "autobahn";
+import * as constants from "./constants";
+import { substituteEndpoint } from "./utils";
+
 // import TurnChooser from "./TurnChooser";
 import "./App.css";
+// const autobahn = require("autobahn");
 
 class App extends Component {
-  constructor() {
-    super(); 
-
-    // hardoced for now
-    this.game_id = 1 ;
-    this.agent_id = 1;
-    this.gameStarted = false;
-
-    // starting Autobahn connection
-    //hardcoded for now
-    //this.connection = new autobahn.Connection({url: 'ws://127.0.0.1:9000/', realm: 'crossbardemo'});
-    //this.connection.onopen = openHander;
+  constructor(props, context) {
+    super(props, context);
+    this.session = null;
+    // currently hardcoding game id and agent id
+    this.gameId = 1;
+    this.agentId = 1;
+    this.state = {
+      actionButton: ""
+    };
   }
 
-  didGameEnd = () => {};
+  componentWillMount() {
+    const url = constants.ROUTER_ENDPOINT;
+    const realm = constants.APPLICATION_REALM;
 
-  startAgent(){
-    //this.connection.open();
-    this.gameStarted = true;
-   }
+    const connection = new Autobahn.Connection({
+      url,
+      realm
+    });
 
-  joinGameCallback(res){
-    this.session.register(res['bsm'], this.getBSMTDecision);
-    this.session.register(res['respondtrade'], this.respondTrade);
-    this.session.register(res['buy'], this.buyProperty);
-    this.session.register(res['auction'], this.auctionProperty);
-    this.session.register(res['jail'], this.jailDecision);
-    this.session.register(res['receivestate'], this.receiveState);
-    this.session.register(res['trade'], this.getTradeDecision);
+    connection.onopen = session => {
+      this.session = session;
+      this.subscribeToEvents();
+    };
 
-    // subsribe for end game results
-    this.session.subscribe(res["endgame"],this.endGame);
+    connection.open();
+  }
 
-    // Successfully Registered. Invoke confirm_register
-    this.session.call(res['confirm_register']).then(
-       function(res){
-          console.log("Result of calling confirm_register: ");
-          console.log(res);
-       }
+  /* Receivers  */
+  receiveTradeRequest = state => {
+    this.setState({ actionButton: constants.TRADE_ACTION });
+    console.log(state);
+  };
+
+  receiveAuctionRequest = state => {
+    this.setState({ actionButton: constants.AUCTION_ACTION });
+    console.log(state);
+  };
+
+  receiveBSMRequest = state => {
+    this.setState({ actionButton: constants.BSM_ACTION });
+    console.log(state);
+  };
+
+  receiveJailDecisionRequest = state => {
+    this.setState({ actionButton: constants.JAIL_DECISION_ACTION });
+    console.log(state);
+  };
+
+  /* Send Response; action listners  */
+
+  sendTradeResponse = event => {
+    this.session.publish(substituteEndpoint(constants.TRADE_PUBLISHER));
+  };
+
+  sendAuctionResponse = event => {
+    this.session.publish(substituteEndpoint(constants.AUCTION_PUBLISHER));
+  };
+
+  sendBSMResponse = event => {
+    this.session.publish(substituteEndpoint(constants.BSM_PUBLISH));
+  };
+
+  sendJailDecisionResponse = event => {
+    this.session.publish(substituteEndpoint(constants.JAIL_PUBLISHER));
+  };
+
+  subscribeToEvents = () => {
+    const { gameId, agentId } = this;
+    this.session.subscribe(
+      substituteEndpoint(constants.TRADE_RECEIVER, agentId, gameId),
+      this.receiveTradeRequest
     );
-  }
+    this.session.subscribe(
+      substituteEndpoint(constants.AUCTION_RECEIVER, agentId, gameId),
+      this.receiveAuctionRequest
+    );
+    this.session.subscribe(
+      substituteEndpoint(constants.BSM_RECEIVER, agentId, gameId),
+      this.receiveBSMRequest
+    );
+    this.session.subscribe(
+      substituteEndpoint(constants.JAIL_RECEIVER, agentId, gameId),
+      this.receiveJailDecisionRequest
+    );
+  };
 
-  openHander(session, details){
-    console.log("Human Player with id: "+this.agent_id+" connected");
-    this.session = session
-    let join_game_uri = 'com.game{}.joingame'.replace('{}',this.game_id)
-    session.call(join_game_uri).then(this.joinGameCallback);
-  }
-
-  endGame(result){
-    console.log("Game has ended");
-    console.log(result);
-  }
-
-  getBSMTDecision(state){
-    console.log("Inside getBSMTDecision");
-    console.log(state);
-    //TODO
-  }
-
-  respondTrade(state){
-    console.log("Inside respondTrade");
-    console.log(state);
-  }
-
-  buyProperty(state){
-    console.log("Inside buyProperty");
-    console.log(state);
-  }
-
-  auctionProperty(state){
-    console.log("Inside auctionProperty");
-    console.log(state);
-  }
-
-  jailDecision(state){
-    console.log("Inside jailDecision");
-    console.log(state);
-  }
-
-  receiveState(state){
-    //very important. update UI
-    console.log("Inside receiveState");
-    console.log(state);
-  }
-
-  getTradeDecision(state){
-    console.log("Inside getTradeDecision");
-    console.log(state);
-  }
+  startGame = () => {
+    this.session.publish("monopoly.auction", ["Start Game"]);
+  };
 
   render() {
+    const { actionButton } = this.state;
+    const {
+      sendAuctionResponse,
+      sendBSMResponse,
+      sendJailDecisionResponse,
+      sendTradeResponse
+    } = this;
     return (
       <div className="App">
-        <button type="button" className="center-block start-game btn btn-primary">
-          Start Game
+        <h1>Welcome to Monopoly </h1>
+        <button onClick={this.startGame}> Start Game </button>
+        <button
+          onClick={sendTradeResponse}
+          className="trade"
+          disabled={actionButton === "trade" ? "" : "disabled"}
+        >
+          Trade
+        </button>
+        <button
+          onClick={sendAuctionResponse}
+          className="auction"
+          disabled={actionButton === "auction" ? "" : "disabled"}
+        >
+          Auction
+        </button>
+        <button
+          onClick={sendBSMResponse}
+          className="bsm"
+          disabled={actionButton === "bsm" ? "" : "disabled"}
+        >
+          BSM
+        </button>
+        <button
+          className="jail-decision"
+          disabled={actionButton === "jail-decision" ? "" : "disabled"}
+          onClick={sendJailDecisionResponse}
+        >
+          Jail Decision
         </button>
         <Board />
       </div>
