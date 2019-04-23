a7b4a1e852ad16b63eb4291da230afe0a9551fc1
varun93 <1hvarun@gmail.com>
2019-04-22 20:38:52 -0400

    rewrote components using useState

--- web-ui/src/components/SpaceDetailed/AuctionPropertyForm.js
+++ web-ui/src/components/SpaceDetailed/AuctionPropertyForm.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 const AuctionPropertyForm = ({ auctionEndpoint, handleClose }) => {
+  const [bid, setBid] = useState(0);
   return (
     <Container>
       <Form.Group controlId="autionProperty">
@@ -11,13 +12,15 @@ const AuctionPropertyForm = ({ auctionEndpoint, handleClose }) => {
         <Form.Control
           min={0}
           type="number"
+          value={bid}
+          onChange={event => setBid(event.target.value)}
           placeholder="Enter an amount to bid"
         />
       </Form.Group>
       <Button
         variant="danger"
         onClick={() => {
-          window.session.publish(auctionEndpoint, [0]);
+          window.session.publish(auctionEndpoint, [bid]);
           handleClose();
         }}
       >
--- web-ui/src/components/SpaceDetailed/index.js
+++ web-ui/src/components/SpaceDetailed/index.js
@@ -1,148 +1,124 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import Modal from "react-bootstrap/Modal";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import SpaceInfo from "./space-info";
+import SpaceInfo from "./SpaceInfo";
 import { setFormData, togglePropertyModal } from "redux/actions";
 import BuyPropertyForm from "./BuyPropertyForm";
 import AuctionPropertyForm from "./AuctionPropertyForm";
 
-class SpaceDetailed extends Component {
-  state = {
-    mortaged: false,
-    housesBought: 0,
-    housesSold: 0
+const SpaceDetailed = ({
+  selectedPropertyIndex,
+  showPropertyModal,
+  properties,
+  candidates,
+  phase,
+  playerAction,
+  buyOutEndpoint,
+  auctionEndpoint,
+  togglePropertyModal,
+  setFormData
+}) => {
+  const handleClose = () => {
+    togglePropertyModal(false);
   };
 
-  handleClose = () => {
-    this.props.togglePropertyModal(false);
-  };
+  const space = properties[selectedPropertyIndex];
+  const [housesBought, setHousesBought] = useState(0);
+  const [housesSold, setHousesSold] = useState(0);
+  const [mortaged, setMortgaged] = useState(false);
 
-  saveChanges = propertyId => {
-    const { housesBought, housesSold, mortaged, propertyBought } = this.state;
-    this.props.setFormData(propertyId, {
-      housesBought,
-      housesSold,
-      mortaged,
-      propertyBought
-    });
-    //now dispatch an action against a property id
-    this.setState({ show: false });
-  };
+  const highlighted =
+    candidates.indexOf(selectedPropertyIndex) !== -1 ? true : false;
 
-  onHousesBrought = event => {
-    this.setState({ housesBought: event.target.value });
-  };
-
-  onHousesSold = event => {
-    this.setState({ housesSold: event.target.value });
-  };
-
-  onMortgaged = event => {
-    this.setState({ mortaged: event.target.checked });
-  };
-
-  render() {
-    const { handleClose } = this;
-    const { housesBought, housesSold, mortaged } = this.state;
-    const {
-      selectedPropertyIndex,
-      showPropertyModal,
-      properties,
-      candidates,
-      phase,
-      playerAction,
-      buyOutEndpoint,
-      auctionEndpoint
-    } = this.props;
-    const { onHousesBrought, onMortgaged, onHousesSold, saveChanges } = this;
-    const space = properties[selectedPropertyIndex];
-    const highlighted =
-      candidates.indexOf(selectedPropertyIndex) !== -1 ? true : false;
-
-    return (
-      showPropertyModal && (
-        <Modal show={showPropertyModal} onHide={handleClose}>
-          <Modal.Header style={{ background: space.monopoly }} closeButton>
-            <Modal.Title>{space.name}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Container>
-              {/* Space info */}
-              <SpaceInfo space={space} />
-              {/* Buy Property Form */}
-              {phase === "buy_property" && (
-                <BuyPropertyForm
-                  buyOutEndpoint={buyOutEndpoint}
-                  handleClose={handleClose}
+  return (
+    showPropertyModal && (
+      <Modal show={showPropertyModal} onHide={handleClose}>
+        <Modal.Header style={{ background: space.monopoly }} closeButton>
+          <Modal.Title>{space.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            {/* Space info */}
+            <SpaceInfo space={space} />
+            {/* Buy Property Form */}
+            {phase === "buy_property" && (
+              <BuyPropertyForm
+                buyOutEndpoint={buyOutEndpoint}
+                handleClose={handleClose}
+              />
+            )}
+            {/* Auciton Property Form */}
+            {phase === "auction_property" && (
+              <AuctionPropertyForm
+                auctionEndpoint={auctionEndpoint}
+                handleClose={handleClose}
+              />
+            )}
+            {/* Form Section */}
+            {highlighted && playerAction === "buy-constructions" && (
+              <Form.Group controlId="formBuyConstructions">
+                <Form.Label>Buy Constructions</Form.Label>
+                <Form.Control
+                  min={space.houses}
+                  max={4}
+                  onChange={event => setHousesBought(event.target.value)}
+                  type="number"
+                  value={housesBought || 0}
+                  placeholder="Constructions to Buy"
                 />
-              )}
-              {/* Auciton Property Form */}
-              {phase === "auction_property" && (
-                <AuctionPropertyForm
-                  auctionEndpoint={auctionEndpoint}
-                  handleClose={handleClose}
+              </Form.Group>
+            )}
+
+            {highlighted && playerAction === "sell-constructions" && (
+              <Form.Group controlId="formSellConstructions">
+                <Form.Label>Sell Constructions</Form.Label>
+                <Form.Control
+                  onChange={event => setHousesSold(event.target.value)}
+                  type="number"
+                  min={0}
+                  max={space.houses}
+                  value={housesSold || 0}
+                  placeholder="Constructions to Sell"
                 />
-              )}
-              {/* Form Section */}
-              {highlighted && playerAction === "buy-constructions" && (
-                <Form.Group controlId="formBuyConstructions">
-                  <Form.Label>Buy Constructions</Form.Label>
-                  <Form.Control
-                    min={space.houses}
-                    max={4}
-                    onChange={onHousesBrought}
-                    type="number"
-                    value={housesBought || 0}
-                    placeholder="Constructions to Buy"
-                  />
-                </Form.Group>
-              )}
+              </Form.Group>
+            )}
 
-              {highlighted && playerAction === "sell-constructions" && (
-                <Form.Group controlId="formSellConstructions">
-                  <Form.Label>Sell Constructions</Form.Label>
-                  <Form.Control
-                    onChange={onHousesSold}
-                    type="number"
-                    min={0}
-                    max={space.houses}
-                    value={housesSold || 0}
-                    placeholder="Constructions to Sell"
-                  />
-                </Form.Group>
-              )}
-
-              {highlighted && playerAction === "mortage-unmortgage" && (
-                <Form.Group controlId="formMortgageProperty">
-                  <Form.Check
-                    onChange={onMortgaged}
-                    checked={mortaged}
-                    type="checkbox"
-                    label="Mortgage Property"
-                  />
-                </Form.Group>
-              )}
-            </Container>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button
-              variant="primary"
-              onClick={() => saveChanges(selectedPropertyIndex)}
-            >
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      )
-    );
-  }
-}
+            {highlighted && playerAction === "mortage-unmortgage" && (
+              <Form.Group controlId="formMortgageProperty">
+                <Form.Check
+                  onChange={event => setMortgaged(event.target.checked)}
+                  checked={mortaged}
+                  type="checkbox"
+                  label="Mortgage Property"
+                />
+              </Form.Group>
+            )}
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() =>
+              setFormData(selectedPropertyIndex, {
+                housesBought,
+                housesSold,
+                mortaged
+              })
+            }
+          >
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  );
+};
 
 const mapDispatchToProps = dispatch => {
   return {
