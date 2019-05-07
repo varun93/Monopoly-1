c814f932a84decbec94e20502148fcf67f656530
Sanjay Mathew Thomas <sanjaythomas.mec@gmail.com>
2019-05-07 15:34:59 -0400

    added landin page based on media-rank,reworking docker-compose

--- adjudicator/game_gen.py
+++ adjudicator/game_gen.py
@@ -147,7 +147,7 @@ class GameGen(ApplicationSession):
 
 if __name__ == '__main__':
 	import six
-	url = environ.get("CBURL", u"ws://127.0.0.1:80/ws")
+	url = environ.get("CBURL", u"ws://127.0.0.1:3000/ws")
 	if six.PY2 and type(url) == six.binary_type:
 		url = url.decode('utf8')
 	realm = environ.get('CBREALM', u'realm1')
--- adjudicator/sampleAgents/TeamBoardwalk/agent_init.py
+++ adjudicator/sampleAgents/TeamBoardwalk/agent_init.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 		sys.exit("Not enough arguments")
 	from riskyAgent import *
 	# pass
-	url = environ.get("CBURL", u"ws://127.0.0.1:80/ws")
+	url = environ.get("CBURL", u"ws://127.0.0.1:3000/ws")
 	if six.PY2 and type(url) == six.binary_type:
 		url = url.decode('utf8')
 	realm = environ.get('CBREALM', u'realm1')
